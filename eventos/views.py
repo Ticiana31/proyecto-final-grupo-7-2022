3ebc76309e1d21adcfa4f0258bbfc94644c9ad68
Ticiana31 <112106003+Ticiana31@users.noreply.github.com>
2022-08-25 13:08:44 -0300

    Update views.py

--- eventos/views.py
+++ eventos/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings 
 
 
 def Inicio(request):
@@ -28,3 +30,17 @@ def Login(request):
 def Register(request):
     template_name= "register.html"
     return render(request, template_name, {})
+
+def formularioContacto(request):
+    return render(request, "formularioContacto.html")
+
+def contactar (request):
+    if request.method == "POST"
+    asunto = request.POST ["txtAsunto"]
+    mensaje = request.POST ["txtMensaje"] + "/ Email: " + request.POST ["txtMensaje"]
+    email_desde = setting.EMAIL_HOST_USER
+    email_para = [ticiana.31052001@gmail.com]
+    send_email (asunto, mensaje, email_desde, email_para, fail_silently= False)
+    return render (request, "contactoExitoso.html")
+    return render(request, "formularioContacto.html")
+    
